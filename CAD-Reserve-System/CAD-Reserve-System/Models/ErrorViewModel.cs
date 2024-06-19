4e794e82ba197e93614d692d028f633ab18114cb
CMCBIOTECH\samat_t <samat_t@innovex.co.th>
2024-06-19 11:19:00 +0700

    create new file

---
+++ CAD-Reserve-System/CAD-Reserve-System/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CAD_Reserve_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
